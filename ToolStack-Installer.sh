70cb6fb19abe98d01ceb18ce48931934aea26a85
TatumLn <linautatum501@gmail.com>
2025-02-19 09:29:24 +0300

    MAJ IDE Installer!

--- ToolStack-Installer.sh
+++ ToolStack-Installer.sh
@@ -51,14 +51,12 @@ install_package() {
         return 0
     else
     	if [ "$distro" == "debian" ] || [ "$distro" == "ubuntu" ] || [ "$distro" == "linuxmint" ] || [ "$distro" == "kali" ]; then
-        	sudo apt update
-       		# sudo apt install $package -y
+       		sudo apt install $package -y
     	elif [ "$distro" == "centos" ] || [ "$distro" == "rhel" ] || [ "$distro" == "fedora" ]; then
-		sudo dnf update
-       		# sudo dnf install $package -y
+		sudo dnf install $package -y
+		#sudo yum install $package -y
     	elif [ "$distro" == "arch" ]; then
-		sudo pacman -Sy
-		#sudo pacman -S
+		sudo pacman -Sy $package --noconfirm
     	else
         	echo "Distribution inconnue"
 	fi
@@ -73,20 +71,48 @@ echo -e "\033[1;36mDistribution détectée: $DISTRO\033[0m\n"
 IDEs=("Visual Studio Code" "IntelliJ IDEA" "PyCharm" "Sublime Text" "Vim" "Quitter")
 
 # Afficher le menu
-echo -e "\033[1;33mSélectionnez l'IDE que vous souhaitez installer :\033[0m"
+echo -e "\033[1;33mSélectionnez l'Éditeurs de Code / IDE que vous souhaitez installer :\033[0m"
 
 select ide in "${IDEs[@]}"; do
     case $ide in
         "Visual Studio Code")
             echo -e "\nVous avez sélectionné : \033[1;34mVisual Studio Code\033[0m"
-            # Installation pour VS Code
-	    install_package jenkins
+
+	    distro=$(detect_distribution)
+
+            case $distro in
+            	"debian" | "ubuntu" | "linuxmint" | "kali")
+                    echo -e "\033[1;36mInstallation de Visual Studio Code pour Debian/Ubuntu...\033[0m"
+            	    sudo apt-get install wget gpg
+            	    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+            	    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+            	    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+            	    rm -f packages.microsoft.gpg
+            	    sudo apt install apt-transport-https
+            	    sudo apt update
+	    	    sudo apt install code -y
+                    ;;
+                "centos" | "rhel" | "fedora")
+                    echo -e "\033[1;36mInstallation de Visual Studio Code pour CentOS/RHEL/Fedora...\033[0m"
+	    	    sudo sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+            	    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+            	    dnf check-update
+		    sudo dnf install code -y
+                    ;;
+                "arch")
+                    echo -e "\033[1;36mInstallation de Visual Studio Code pour Arch Linux...\033[0m"
+	   	    sudo pacman -Sy code --noconfirm
+                    ;;
+		                *)
+                    echo -e "\033[1;31mDistribution non supportée : $distro\033[0m"
+                    ;;
+            esac
             break
             ;;
         "IntelliJ IDEA")
             echo -e "\nVous avez sélectionné : \033[1;34mIntelliJ IDEA\033[0m"
             # Installation pour IntelliJ IDEA
-	    install_package
+	    install_package 
             break
             ;;
         "PyCharm")
@@ -104,7 +130,7 @@ select ide in "${IDEs[@]}"; do
         "Vim")
             echo -e "\nVous avez sélectionné : \033[1;34mVim\033[0m"
             # Installation pour Vim
-	    install_package
+	    install_package vim
             break
             ;;
         "Quitter")
